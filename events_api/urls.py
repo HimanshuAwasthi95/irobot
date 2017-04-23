aeaa245aa3e558a96acc32f6c14cac14ad5199b2
Akhil Lawrence <akhilputhiry@gmail.com>
2017-04-23 20:36:55 +0530

    updated oauth urls

--- events_api/urls.py
+++ events_api/urls.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from .views import Incoming
+from . import views
 
 
 urlpatterns = [
-    url(r'^incoming/', Incoming.as_view(), name='incoming'),
+    url(r'^incoming/$', views.Incoming.as_view(), name='incoming'),
+    url(r'^oauth/start/$', views.OauthStart.as_view(), name='oauth_start'),
+    url(r'^oauth/finish/$', views.OauthFinish.as_view(), name='oauth_finish'),
 ]
