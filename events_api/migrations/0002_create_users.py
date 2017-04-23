3bc72d7f30f1b9ea214410225635a028fcec2141
Akhil Lawrence <akhilputhiry@gmail.com>
2017-04-23 20:25:06 +0530

    added migrations

---
+++ events_api/migrations/0002_create_users.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.contrib.auth.models import User
+
+
+def create_users(apps, schema_editor):
+    if not schema_editor.connection.alias == 'default':
+        return
+    User(
+        username='akhilputhiry',
+        email='akhilputhiry@gmail.com',
+        first_name='Akhil',
+        last_name='Lawrence',
+        is_staff=True,
+        is_active=True,
+        is_superuser=True
+    ).save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        # Dependencies to other migrations
+    ]
+
+    operations = [
+        migrations.RunPython(create_users),
+    ]
